284e0ed77954efcb1bab67ad6f1cbe519993190b
tugasSBD <ian25yola@gmail.com>
2021-05-25 02:25:41 +0700

    Perbaikan pada settings dan readme

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '0bijc$y1f9@=uh-e77=&(@465=1=v-6vcrj2h^wez%x(qg36w+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-18-220-49-43.us-east-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
